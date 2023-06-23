1c55da5afcf97f1dacb7544f2b1668a9658c73f0
carolina cayo <carolinacayo028@gmail.com>
2023-06-22 20:21:24 -0500

    segundo commit

--- application/views/footer.php
+++ application/views/footer.php
@@ -29,7 +29,7 @@
       </div>
       <div class="col-md-4 text-center">
             <h4>
-              <b>Direccion</b>
+              <b>Direccion del CNNE</b>
             </h4>
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3293.8078102011063!2d-78.63297117298335!3d-0.920579577975466!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x91d461067c9c16f9%3A0x7f177d885f16c955!2sRestaurante%20Mitad%20del%20Mundo!5e0!3m2!1ses!2sec!4v1683760039128!5m2!1ses!2sec" width="50%" height="10%" style="border:1;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
       </div>
